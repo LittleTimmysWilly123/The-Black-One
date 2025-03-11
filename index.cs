c0abfb5e1c6488806139d8106672cb6e0cee06b8
LittleTimmysWilly123 <BrWard@intellectusprep.org>
2025-03-11 13:23:19 +0000

    create index.cs file

---
+++ index.cs
@@ -0,0 +1,10 @@
+using System
+namespace WiggleFoot{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hi")
+            string name1 = "StarLight Wiggle Foot"
+            
+        }
+    }
+}
