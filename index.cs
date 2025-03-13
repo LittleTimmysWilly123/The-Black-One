405ae4f4f62a68a1a1f0ba2c0b723a441879c912
LittleTimmysWilly123 <BrWard@intellectusprep.org>
2025-03-13 06:20:33 -0700

    create index.cs file

--- index.cs
+++ index.cs
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
+using System
+namespace WiggleFoot{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hi")
+        }
+    }
+}
+======
 using System;
 namespace The_Black_One_Name{
     class Program{
@@ -8,3 +18,4 @@ namespace The_Black_One_Name{
         }
     }
 }
+>>>>>>> 101938c695a6f0ee3945f73fbcb3343304c1cd44
